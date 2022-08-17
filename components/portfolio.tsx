58b606a29805d2501ef43efd7bf33c8bbbecb772
lamptechs <career@lamptechs.com>
2022-08-17 19:25:43 +0600

    added

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -1,47 +1,50 @@
-import Project from './project'
+import Project from "./project";
 
 export default function Portfolio() {
-    return (
-        <section className="section is-medium" id="portfolio">
-            <h2 className="title"> Our Portfolio </h2>
-            <br />
-            
-            <div className="columns is-centered">
-                <div className="column is-6-tablet is-6-desktop">
-                    <Project 
-                        projectName="Vertigo File Hosting"
-                        projectImage="/vertigo-project.jpg" 
-                        projectLink="http://vertigo-files.herokuapp.com/" />
-                </div>
-                {/*<div className="column is-6-tablet is-6-desktop">
+  return (
+    <section className="section is-medium" id="portfolio">
+      <h2 className="title"> Our Portfolio </h2>
+      <br />
+
+      <div className="columns is-centered">
+        <div className="column is-6-tablet is-6-desktop">
+          <Project
+            projectName="Arshi 365 Ecommerce"
+            projectImage="/arshi.PNG"
+            projectLink="https://mahmudhasan-test-4dzlnukip-mahmudul165.vercel.app/"
+          />
+        </div>
+        {/*<div className="column is-6-tablet is-6-desktop">
                     <Project 
                         projectName="Mindmap Education Consultancy"
                         projectImage="/mindmap-project.jpg" 
                         projectLink="http://mindmapedu.co.uk/" />
                 </div>*/}
-                <div className="column is-6-tablet is-6-desktop">
-                    <Project 
-                        projectName="Atay Trade"
-                        projectImage="/atay-project.jpg" 
-                        projectLink="https://www.ataytrade.co.uk/" />
-                </div>
-            </div>
+        <div className="column is-6-tablet is-6-desktop">
+          <Project
+            projectName="Automation process for a psychotherapy hospital"
+            projectImage="/atay-project.jpg"
+            projectLink="https://misi-lamptechs-bd.vercel.app/"
+          />
+        </div>
+      </div>
 
-            <div className="columns is-centered">
-                <div className="column is-6-tablet is-6-desktop">
-                    <Project 
-                        projectName="Medikey Health Consultancy"
-                        projectImage="/medikey-project.jpg" 
-                        projectLink="https://www.medikeyhealth.co.uk/" />
-                </div>
-                <div className="column is-6-tablet is-6-desktop">
-                        <Project 
-                            projectName="Zeytun ABC Accounting"
-                            projectImage="/zeytun-project.jpg" 
-                            projectLink="https://www.zeytunabc.com/" />
-                </div>
-            </div>
-
-        </section>
-    );
+      <div className="columns is-centered">
+        <div className="column is-6-tablet is-6-desktop">
+          <Project
+            projectName="Influencer hub"
+            projectImage="/influencer.PNG"
+            projectLink="https://influencer-marketplace.vercel.app/"
+          />
+        </div>
+        <div className="column is-6-tablet is-6-desktop">
+          <Project
+            projectName="Regulatory advisor"
+            projectImage="/public/regulatory.PNG"
+            projectLink="https://regulatedadvice.co.uk/"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
